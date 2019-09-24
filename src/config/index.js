669143d7855f31396e45d34ee7d510cee86a2894
Erick Guimaraes <erick.guimaraes@codeminer42.com>
2019-09-24 17:47:18 -0300

    changing port to work with heroku

--- src/config/index.js
+++ src/config/index.js
@@ -5,7 +5,7 @@ const baseConfig = {
   env,
   isDev: env === 'development',
   isTest: env === 'testing',
-  port: 3000,
+  port: process.env.PORT || 3000,
   secrets: {
     jwt: process.env.JWT_SECRET,
     jwtExp: '100d'
